aeccfad596d8591e41b9dccc1ea77eff033bc187
JJtankevans <vitorflamengao2@gmail.com>
2025-02-11 15:16:35 -0400

    feature:home-fragment-layout

---
+++ app/src/main/java/com/vitoroliveira/planner/ui/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.vitoroliveira.planner.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vitoroliveira.planner.R
+import com.vitoroliveira.planner.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            //TODO: logica da tela home
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
