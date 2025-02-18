792a5d5605344035432ff91b9915a7b1468daf0c
JJtankevans <vitorflamengao2@gmail.com>
2025-02-18 14:49:15 -0400

    feature:creating-base-token-management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.datastore)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.datastore.preferences)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
---
+++ app/src/main/java/com/vitoroliveira/planner/data/datasource/AuthenticationLocalDataSource.kt
@@ -0,0 +1,11 @@
+package com.vitoroliveira.planner.data.datasource
+
+import kotlinx.coroutines.flow.Flow
+
+interface AuthenticationLocalDataSource {
+    val token: Flow<String>
+
+    val expirationDateTime: Flow<Long>
+
+    suspend fun insertToken(token: String)
+}
